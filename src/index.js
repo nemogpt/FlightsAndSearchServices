0c894646d21cd868b5d5134e20814c35c873123c
NAMAN GUPTA <namangupta990@gmail.com>
2023-07-29 15:47:05 +0530

    completed db config

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ const {PORT}=require('./config/serverConfig');
 
 
 
-const setupAndStartServer=async()=>{
-    const app=express();
+const setupAndStartServer = async() => {
+    const app = express();
 
 
-    app.use(bodyPareser.json());
+    app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
