d3460ee7b3144f6857a09cbb09ca0030ec18bb7d
NAMAN GUPTA <namangupta990@gmail.com>
2023-07-30 15:56:19 +0530

    completed city repo

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ const bodyParser=require("body-parser");
 const {PORT}=require('./config/serverConfig');
 
 
-
 const setupAndStartServer = async() => {
     const app = express();
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
+        
      
         console.log(`Server started at ${PORT}`);
     })
--- src/repository/city_repository.js
+++ src/repository/city_repository.js
@@ -2,30 +2,74 @@ const {City} =require('../models/index');
 
 
 class CityRepository{
-    async createCity({name}){ // we need object as params bt we destructing the object
-        try{
-            const city=await City.create({name});
-            return city;
 
-        }catch(error){
-            throw {error};
 
-        }
+   async createCity({name}){ // destructuring the obj
+    try{
+        const city = await City.create({
+            name:name //LHS->model property name 
+            //RHS THE ACTUYAL NAME THAT HAS BEEN PASSED
+            
+        })
+        return city;
+
+    }
+    catch(error){
+        console.log("Something went wrong in repository layer");
+        throw {error};
+
     }
 
-        async deleteCity({cityId}){
-            try{
-                await City.destroy({
-                where:{
-                    id:cityId 
-                }
-            });
+   }
 
-            }catch(error){
-                throw {error};
-
+   async deleteCity(cityId){
+    try{
+        await City.destroy({
+            where:{
+                id:cityId
             }
-        }
+        });
+        return true;
+
+    }
+    catch(error){
+        console.log("Something went wrong in repository layer");
+        throw {error};
+        
+    }
+
+   }
+   async updateCity(cityId,data){
+    try{
+        const city= await City.update(data,{
+            where:{
+                id:cityId
+            }
+        });
+        return city;
+
+    }
+    catch(error){
+        console.log("Something went wrong in repository layer");
+        throw {error};
+        
+    }
+
+   }
+
+   async getCity(cityId){
+    try{
+        const city=await City.findByPk(cityId);
+        return city;
+
+    }
+    catch(error){
+        console.log("Something went wrong in repository layer");
+        throw {error};
+        
+    }
+
+   }
 }
 
 
