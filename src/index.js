d4dd4e0f47029ac0a4dc4e6bc3118cc67d38c418
NAMAN GUPTA <namangupta990@gmail.com>
2023-07-30 15:24:27 +0530

    Added unique constraint and added cityRepository

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
- const express=require('express');
+const express=require('express');
 const bodyParser=require("body-parser");
-
-
-
 const {PORT}=require('./config/serverConfig');
 
 
@@ -10,10 +7,10 @@ const {PORT}=require('./config/serverConfig');
 const setupAndStartServer = async() => {
     const app = express();
 
-
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
+     
         console.log(`Server started at ${PORT}`);
     })
 
